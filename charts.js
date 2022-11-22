77af8c4aa91d1572901a33efb4afffca50a20e12
Md Mahmudunnobe <mahmud.nobe@edu.minerva.edu>
2022-11-21 23:17:32 -0500

    create gauge chart with washing frequency

--- charts.js
+++ charts.js
@@ -117,5 +117,36 @@ function buildCharts(sample) {
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot('bubble', bubbleData, bubbleLayout);
 
+
+    // Gauge Chart:
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata_for_sample = data.metadata.filter(obj => obj.id === parseInt(sample))[0]
+    
+    // Get their wash frequency
+    var wfreq = metadata_for_sample.wfreq;
+    
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      value: wfreq,
+      type: 'indicator',
+      mode: 'gauge+number',
+      gauge: {
+        axis: {range: [0,10]},
+        bar: { color: "black" },
+        steps: [
+          { range: [0, 2], color: "red" },
+          { range: [2, 4], color: "orange" },
+          { range: [4, 6], color: "yellow" },
+          { range: [6, 8], color: "green" },
+          { range: [8, 10], color: "blue" }
+        ]
+      },
+      title: {text: '<b> Washing Frequency</b> <br> Scrubs per Week'}
+    }];
+    
+    // 5. Create the layout for the gauge chart.
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot('gauge', gaugeData);
   });
 }
