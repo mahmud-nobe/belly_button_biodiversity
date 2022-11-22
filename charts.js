bda27e9d9bb0a6f7c3563f6e96dcdcf3ea390a01
Md Mahmudunnobe <mahmud.nobe@edu.minerva.edu>
2022-11-21 20:32:37 -0500

    select the specific bac samples for given id

--- charts.js
+++ charts.js
@@ -58,11 +58,12 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var my_sample = samples.filter(obj => obj.id === sample);
     //  5. Create a variable that holds the first sample in the array.
-
+    var firstSample = my_sample[0];
+    console.log(firstSample);
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
 
@@ -71,16 +72,16 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    //var yticks = 
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
+    //var barData = [
       
-    ];
+    //];
     // 9. Create the layout for the bar chart. 
-    var barLayout = {
+    //var barLayout = {
      
-    };
+    //};
     // 10. Use Plotly to plot the data with the layout. 
     
   });
